7513a6f48aa54f770fa651ff39e82e13546b6cac
SzeHanTan <147508070+SzeHanTan@users.noreply.github.com>
2025-05-05 23:37:57 +0800

    create speciality menu for home page

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { specialityData } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const SpecialityMenu = () => {
+  return (
+    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
+        <h1>Find by Speciality</h1>
+        <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+        <div>
+            {specialityData.map((item,index)=>(
+                <Link key={index} to={`/doctors/${item.speciality}`}>
+                    <img src={item.image} alt="" />
+                    <p>{item.speciality}</p>
+                </Link>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default SpecialityMenu
