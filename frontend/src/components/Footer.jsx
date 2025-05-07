1f485c76a0f9e45170748931bad46ca2de3fa5bd
SzeHanTan <147508070+SzeHanTan@users.noreply.github.com>
2025-05-08 00:40:11 +0800

    add css for footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
             {/* ---- Left Section ---- */}
             <div>
-                <img src={assets.logo} alt="" />
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <img className='mb-5 w-40' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
 
             {/* ---- Center Section ---- */}
             <div>
-                <p>COMPANY</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Contact us</li>
@@ -26,8 +26,8 @@ const Footer = () => {
 
             {/* ---- Right Section ---- */}
             <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+6013-4567899</li>
                     <li>appointmentadmin@gmail.com</li>
                 </ul>
@@ -38,7 +38,7 @@ const Footer = () => {
         {/* ---- Copyright Text ---- */}
         <div>
             <hr />
-            <p>Copyright © 2025 AppointmentSystem - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright © 2025 AppointmentSystem - All Right Reserved.</p>
         </div>
 
     </div>
